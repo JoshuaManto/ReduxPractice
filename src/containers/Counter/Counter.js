57da943d563fe5c8f7194da20b945ff7c84935c6
JoshuaManto <joshuamanto@yahoo.com>
2019-01-12 18:48:18 -0800

    Added the immutable deletion of a value from the array in the state.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -56,7 +56,10 @@ class Counter extends Component {
         <button onClick={this.props.onStoreResult}>Store Result</button>
         <ul>
           {this.props.storedResults.map(strResult => (
-            <li key={strResult.id} onClick={this.props.onDeleteResult}>
+            <li
+              key={strResult.id}
+              onClick={() => this.props.onDeleteResult(strResult.id)}
+            >
               {strResult.value}
             </li>
           ))}
@@ -80,7 +83,8 @@ const mapDispatchToProps = dispatch => {
     onAddCounter: () => dispatch({ type: 'ADD', val: 10 }),
     onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 10 }),
     onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-    onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' })
+    onDeleteResult: id =>
+      dispatch({ type: 'DELETE_RESULT', resultElementId: id })
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -40,6 +40,19 @@ const reducer = (state = initialState, action) => {
         // Using concat is an immutable way of manipulating an array
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
+    case 'DELETE_RESULT':
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElementId
+      );
+
+      return {
+        ...state,
+        results: updatedArray
+      };
   }
 
   // if (action.type === 'INCREMENT') {
