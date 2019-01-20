20e468897b661db75406c4379f842953f9f84681
JoshuaManto <joshuamanto@yahoo.com>
2019-01-20 10:57:46 -0800

    Refactored actions to separate them based on type and purpose (counter and result). Modified import calls.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import * as actionTypes from '../../store/actions/actions';
 // import { increment } from '../../store/actions/actions';
-import * as actionCreators from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/index';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
